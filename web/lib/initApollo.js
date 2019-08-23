cbefcb459a20b891c82ff2cf9de085f98a22a631
amodinho <u14284783@tuks.co.za>
2019-08-23 16:39:59 +0200

    done with init apollo

--- web/lib/initApollo.js
+++ web/lib/initApollo.js
@@ -16,6 +16,36 @@ function create (initalState, {fetchOptions}){
         credentials: 'same-origin',
         fetchOptions
     })
+
+    const isBrowser = typeof window !== 'undefined'
+    return new ApolloClient({
+    connectToDevTools: isBrowser,
+    ssrMode: !isBrowser, //this will make sure queires only run once
+    cache: new InMemoryCache().restore(initialState || {})
+})
 }
 
 
+export default function initApollo (initalState, options){
+    //new client is made for every server-side request
+    if (typeof window === 'undefined'){
+        let fetchOptions = {}
+        //fetchOptions is required because its  a server-side only module
+        if (process.env.https_proxy){
+            fetchOptions = {
+                agent: new (require('https-proxy-agent'))(process.env.https_proxy)
+            }
+        }
+        return create(initialState,{
+            ...options,
+            fetchOptions
+        })
+    }
+
+    ///use the client on the client-side
+    if(!apolloClient){
+        apolloClient = create(initalState, options)
+    }
+
+    return apolloClient
+}
