0a2fecb633600cf95efc1a3acd1cf83d94380ffd
amodinho <u14284783@tuks.co.za>
2019-08-23 16:45:56 +0200

    imported libs

---
+++ web/lib/withApollo.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import cookie from 'cookie'
+import PropTypes from 'prop-types'
+import {getDataFromTree} from '@apollo/react-ssr'
+import Head from 'next/head'
+import initApollo from './initApollo'
