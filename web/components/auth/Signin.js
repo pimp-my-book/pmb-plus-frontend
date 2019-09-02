b2dff5181cdf902faed3be4f78b2888d0baf0410
AmoDinho <u14284783@tuks.co.za>
2019-09-02 15:02:48 +0200

    importing libs

--- web/components/auth/Signin.js
+++ web/components/auth/Signin.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import { HeadingOne, Input, LinkButton, BodyText } from 'umqombothi-component-library'
 
 const Signin = ({
 
 }) => {
+
+
     return (
         <>
-            Hi
+            <div>
+                <form
+                    className="w-full max-w-lg xl:h-500 shadow-lg mt-10"
+                >
+
+                </form>
+            </div>
         </>
     )
 }
