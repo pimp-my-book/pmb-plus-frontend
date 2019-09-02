49fe818b99bf5daf0b3c3daf76e741cc70d668d5
AmoDinho <u14284783@tuks.co.za>
2019-09-02 15:46:57 +0200

    added signin test

---
+++ web/__tests__/Signin.test.js
@@ -0,0 +1,13 @@
+describe('<Signin/>', () => {
+    it('Renders the page', () => {
+
+    })
+
+    it('Logs the user in', () => {
+
+    })
+
+    it('Sends error message when not logged in', () => {
+
+    })
+})
