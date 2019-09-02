76ed3e0423dbcb9812abf9743a511c97af97a41b
AmoDinho <u14284783@tuks.co.za>
2019-09-02 14:39:09 +0200

    removing graphql from the index

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,19 +1,13 @@
-import { useQuery } from '@apollo/react-hooks'
 import { BodyText } from 'umqombothi-component-library'
-import { HELLO } from '../graphql/Queries'
+
 
 function Home() {
 
-    const { loading, error, data } = useQuery(HELLO)
-
-    if (loading) return <><p>...loading</p>  </>
-    if (error) return <> <p>Something went wrong</p> <BodyText text="me" /> </>
-    console.log(data)
 
     return (
         <>
             <div>PMB Plus Front-End! 🤩 </div>
-            <p>{data.hello}</p>
+
         </>
     )
 }
