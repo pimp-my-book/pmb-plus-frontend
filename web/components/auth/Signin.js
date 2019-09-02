9292adc937d7e8d472dc1a763bccd00c5312f6c1
AmoDinho <u14284783@tuks.co.za>
2019-09-02 15:04:06 +0200

    adding components

--- web/components/auth/Signin.js
+++ web/components/auth/Signin.js
@@ -13,6 +13,13 @@ const Signin = ({
                 <form
                     className="w-full max-w-lg xl:h-500 shadow-lg mt-10"
                 >
+                    <HeadingOne
+                        text="Welcome Back"
+                    />
+
+                    <Input
+                        placeholder="email@example.com"
+                    />
 
                 </form>
             </div>
