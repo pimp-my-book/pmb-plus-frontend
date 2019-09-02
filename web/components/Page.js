43c08afb2d68e58cb413fec9df743605bde65616
AmoDinho <u14284783@tuks.co.za>
2019-09-02 14:43:56 +0200

    done with page.js

---
+++ web/components/Page.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Footer, NavigationBar } from 'umqombothi-component-library'
+
+class Page extends Component {
+    render() {
+        return (
+            <>
+                <NavigationBar />
+                {this.props.children}
+                <Footer />
+            </>
+        )
+    }
+}
