2aafb97c561fad75ed4c1aa531d46a25403f358d
AmoDinho <u14284783@tuks.co.za>
2019-09-02 14:46:21 +0200

    added nav and footer

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -2,6 +2,7 @@ import App from 'next/app'
 import React from 'react'
 import { ApolloProvider } from '@apollo/react-hooks'
 import withApollo from '../lib/withApollo'
+import Page from '../components/Page'
 
 class MyApp extends App {
     static displayName = 'PMB Plus'
@@ -10,7 +11,9 @@ class MyApp extends App {
         const { Component, pageProps, apolloClient } = this.props
         return (
             <ApolloProvider client={apolloClient}>
-                <Component {...pageProps} />
+                <Page>
+                    <Component {...pageProps} />
+                </Page>
             </ApolloProvider>
         )
     }
