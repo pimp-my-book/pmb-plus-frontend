cd9994edc0100bd487e64e4666ea3ff7d706f6e0
AmoDinho <u14284783@tuks.co.za>
2019-09-02 15:48:21 +0200

    addded cofig file

--- web/config.js
+++ web/config.js
@@ -1,2 +1,29 @@
-export const endpoint = `http://localhost:4000/graphql`
-export const devEndpoint = `https://2m3x5565db.execute-api.us-east-1.amazonaws.com/dev/graphql`
+const dev = {
+
+    cognito: {
+        REGION: process.env.REACT_APP_REGION,
+        USER_POOL_ID: process.env.REACT_APP_UserPoolID_Dev,
+        APP_CLIENT_ID: process.env.REACT_APP_UserPoolClientID_Dev,
+        IDENTITY_POOL_ID: process.env.REACT_APP_IdentityPoolId_Dev,
+
+    }
+};
+
+const prod = {
+
+    cognito: {
+        REGION: process.env.REACT_APP_REGION,
+        USER_POOL_ID: process.env.REACT_APP_UserPoolID_PROD,
+        APP_CLIENT_ID: process.env.REACT_APP_UserPoolClientID_PROD,
+        IDENTITY_POOL_ID: process.env.REACT_APP_IdentityPoolId_PROD,
+    }
+};
+
+const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+
+
+export default {
+    ...config
+};
