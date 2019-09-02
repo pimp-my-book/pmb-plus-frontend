b749314b6b0c1eb919118f8c960274a0f8f96e65
AmoDinho <u14284783@tuks.co.za>
2019-09-02 13:55:49 +0200

    testing component

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,11 +1,12 @@
 import { useQuery } from '@apollo/react-hooks'
+import { BodyText } from 'umqombothi-component-library'
 import { HELLO } from '../graphql/Queries'
 
 function Home() {
 
     const { loading, error, data } = useQuery(HELLO)
 
-    if (loading) return <p>...loading</p>
+    if (loading) return <><p>...loading</p> <BodyText text="me" />  </>
     if (error) return <p>Something went wrong</p>
     console.log(data)
 
