f878cb7f8f760f21f34c05b3554ca0c22c2bd36b
AmoDinho <u14284783@tuks.co.za>
2019-09-05 16:57:47 +0200

    trying to authenticate uses

--- web/lib/withAuth.js
+++ web/lib/withAuth.js
@@ -1,1 +1,30 @@
 import React from 'react'
+import redirect from './redirect'
+import Auth from "@aws-amplify/auth";
+
+export default withAuth => {
+    return class AuthComponent extends React.Component {
+        static async getIntialProps(ctx) {
+            let authProps = {
+                isAuthenticated: false
+            }
+
+            if (withAuth.getIntialProps) {
+
+                try {
+                    if (await Auth.currentSession()) {
+                        appProps.isAuthenticated = true
+                    }
+
+                }
+                catch (e) {
+                    return e.message
+                }
+            }
+
+
+
+
+        }
+    }
+}
