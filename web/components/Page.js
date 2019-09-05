153829d25bbbaea52deca18fb5ec0e24608a5ce9
AmoDinho <u14284783@tuks.co.za>
2019-09-05 14:24:23 +0200

    adding logout functionallity

--- web/components/Page.js
+++ web/components/Page.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import { Footer, NavigationBar } from 'umqombothi-component-library'
+import Auth from "@aws-amplify/auth";
 
 
 class Page extends Component {
 
+    handleLogout = async event => {
+        await Auth.signOut()
+
+    }
     render() {
 
 
