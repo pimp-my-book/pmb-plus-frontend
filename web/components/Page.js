af2cd466730975699d6933e06d5561a2acecfe75
AmoDinho <u14284783@tuks.co.za>
2019-09-05 14:48:56 +0200

    moved the config

--- web/components/Page.js
+++ web/components/Page.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
 import { Footer, NavigationBar } from 'umqombothi-component-library'
+import Amplify from "@aws-amplify/core";
 import Auth from "@aws-amplify/auth";
+import config from '../config'
 
+const amplifyConfig = {
+    Auth: {
+        mandatorySignIn: false,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    }
+}
+
+
+Amplify.configure(
+    amplifyConfig
+)
 
 class Page extends Component {
 
