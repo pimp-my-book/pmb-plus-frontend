11ab134ac906c144802565c425c9695e518437bd
AmoDinho <u14284783@tuks.co.za>
2019-09-09 17:21:11 +0200

    added fire events

--- web/__tests__/Signin.test.js
+++ web/__tests__/Signin.test.js
@@ -1,15 +1,18 @@
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, getByLabelText } from '@testing-library/react'
 import Signin from '../components/auth/Signin'
 
 describe('<Signin/>', () => {
 
     const handleSubmit = jest.fn()
     it('Logs the user in', () => {
-        const { getByText, findByText } = render(
+        const { getByText, findByText, getBy } = render(
             <Signin />
         )
 
+        fireEvent.change(getByLabelText(/email address/i), { target: { value: 'vendor@example.com' } })
+        fireEvent.change(getByLabelText(/password/i), { target: { value: 'rassword!' } })
 
+        fireEvent.click(getByText(/login/i))
     })
 
     it('Sends error message when not logged in', () => {
