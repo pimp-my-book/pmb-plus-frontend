0958445d724c2c58e3d944fb897efa1c3e424754
AmoDinho <u14284783@tuks.co.za>
2019-09-10 14:41:57 +0200

    done with test case

--- web/__tests__/Signin.test.js
+++ web/__tests__/Signin.test.js
@@ -13,10 +13,19 @@ describe('<Signin/>', () => {
         fireEvent.change(getByLabelText(/password/i), { target: { value: 'rassword!' } })
 
         fireEvent.click(getByText(/login/i))
-        expectExport(handleSubmit).toHaveBeenCalledTimes(1)
+        expect(handleSubmit).toHaveBeenCalledTimes(1)
     })
 
-    it.skip('Sends error message when not logged in', () => {
+    it('Sends error message when not logged in', () => {
+        const { getByText, findByText, getBy } = render(
+            <Signin />
+        )
 
+        fireEvent.change(getByLabelText(/email address/i), { target: { value: 'vendor@example.com' } })
+        fireEvent.change(getByLabelText(/password/i), { target: { value: 'rassword!' } })
+
+        fireEvent.click(getByText(/login/i))
+
+        expect(getByText(/user does not exist/))
     })
 })
