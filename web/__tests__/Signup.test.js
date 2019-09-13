750b66ec14538af31532a2deaef3ad2d67bc7f17
AmoDinho <u14284783@tuks.co.za>
2019-09-13 17:02:07 +0200

    added test for signup

---
+++ web/__tests__/Signup.test.js
@@ -0,0 +1,15 @@
+import { render, fireEvent, getByLabelText } from '@testing-library/react'
+
+describe('<Signup/>', () => {
+    it('Allows the user to sign up', () => {
+
+    })
+
+    it('Does not allow form to be submitted if fields not validated', () => {
+
+    })
+
+    it('Shows an error if something went wrong', () => {
+
+    })
+})
