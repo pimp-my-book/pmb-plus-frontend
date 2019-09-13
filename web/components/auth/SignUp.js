f25e86ca07385706f9c67251c91b55c8059bb6f9
AmoDinho <u14284783@tuks.co.za>
2019-09-13 17:03:19 +0200

    added sign up form

---
+++ web/components/auth/SignUp.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Auth from "@aws-amplify/auth";
+import Router from 'next/router'
+import { Alert, HeadingOne, Input, LinkButton, BodyText, DarkPinkButton } from 'umqombothi-component-library'
+
+
+const SignUp = ({ }) => {
+    return (
+        <div>
+            Sign Up
+        </div>
+    )
+}
