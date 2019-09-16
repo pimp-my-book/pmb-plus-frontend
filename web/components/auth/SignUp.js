2e610adeb66cc293db3f9991ee8dd79be517e34f
AmoDinho <u14284783@tuks.co.za>
2019-09-16 16:17:35 +0200

    added a line

--- web/components/auth/SignUp.js
+++ web/components/auth/SignUp.js
@@ -19,12 +19,25 @@ const SignUp = ({ }) => {
             </div>
             <div className="flex justify-center mb-20">
                 <form
-                    className="w-full max-w-lg h-400 xl:h-500 shadow-lg mt-10"
+                    className="w-full xl:h-500 shadow-lg mt-10"
                 >
                     <HeadingOne
                         className="text-center"
                         text="Lets get you set up!"
                     />
+                    {/* Personal Info*/}
+                    <div>
+                        <hr
+                            className="border-greyDark"
+                        />
+
+                    </div>
+
+                    {/* Account  Info*/}
+                    <div>
+
+                    </div>
+
 
                 </form>
 
