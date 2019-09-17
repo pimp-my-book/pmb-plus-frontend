cf450489a013bf3a143711b247bad98b81bef0e9
AmoDinho <u14284783@tuks.co.za>
2019-09-17 15:55:35 +0200

    Grid is working

--- web/components/auth/SignUp.js
+++ web/components/auth/SignUp.js
@@ -8,11 +8,13 @@ import { Alert, Textarea, HeadingOne, HeadingFive, Input, LinkButton, BodyText, 
 const SignUp = ({ }) => {
     const [error, setError] = useState("")
 
-    const formGrid = styled.div`
+    const FormGrid = styled.div`
     display: grid;
     grid-template-columns: repeat(2,1fr);
     grid-gap: 30px;
     `
+
+
     return (
         <>
             <div
@@ -37,6 +39,24 @@ const SignUp = ({ }) => {
                         className="border-greyDark"
                     />
 
+                    <FormGrid>
+                        <HeadingFive
+                            text="Personal Info"
+                            className="text-left"
+                        />
+                        {/* Full name*/}
+                        <div>
+                            <BodyText
+                                text="Full Name"
+                            />
+                            <Input
+                                type="text"
+                                required
+                                placeholder="Sizzla Kalonje"
+                            />
+
+                        </div>
+                    </FormGrid>
 
                     {/* Start of left col */}
 
