8b7ccdca11169a5e72ea68cface350bdfd4db902
AmoDinho <u14284783@tuks.co.za>
2019-09-27 16:13:45 +0200

    added comments

--- web/components/auth/ForgotPassword.js
+++ web/components/auth/ForgotPassword.js
@@ -13,16 +13,42 @@ const ForgotPassword = ({ props }) => {
     const [emailError, setEmailError] = useState("")
     //errors to display on ConfirmationForm
     const [confirmError, setConfirmError] = useState("")
+    const FormGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(320px,1fr));
+    grid-gap: 30px;
+    `
 
+
+    /*
+
+This form shows the initial form for the user to 
+request a reset.
+    */
     const RenderRequestCodeForm = () => {
         return (
             <>
-                renderRequestCodeForm
+                <div
+                    className="flex mt-5 justify-center">
+                    {error &&
+                        <Alert
+                            message={error}
+                            error />
+                    }
+                </div>
+                <form>
+
+                </form>
 
             </>
         )
     }
 
+    /*
+
+This form allows the user to enter the code they recieved
+in their inbox and set a new password.
+    */
     const RenderConfirmationForm = () => {
         return (
             <>
@@ -31,6 +57,12 @@ const ForgotPassword = ({ props }) => {
         )
     }
 
+
+
+    /*
+
+This form shows the user that the reset was successfull
+    */
     const RenderSuccessMessage = () => {
         return (
             <>
@@ -42,6 +74,14 @@ const ForgotPassword = ({ props }) => {
 
     return (
         <>
+            {/* 
+        first we ask if the code has been sent - if not 
+        then show RenderRequestCodeForm()
+
+        second we ask if you have not been confirmed, then we
+        show the user  RenderConfirmationForm()
+
+        */}
             {!codeSent ? RenderRequestCodeForm() : !confirmed ? RenderConfirmationForm() : RenderSuccessMessage()}
 
         </>
