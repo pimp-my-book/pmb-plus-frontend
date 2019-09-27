ca5db0ec1e4b810fdf877a259210f544ad59d493
AmoDinho <u14284783@tuks.co.za>
2019-09-27 15:34:52 +0200

    imported librarys

---
+++ web/components/auth/ForgotPassword.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react'
+import Auth from '@aws-amplify/auth'
+import { Alert, HeadingOne, Input, LinkButton, BodyText, DarkPinkButton } from 'umqombothi-component-library'
+
