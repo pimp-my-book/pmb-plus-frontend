85f8120a7ad74d767250c08566b375ec0b3cf661
AmoDinho <u14284783@tuks.co.za>
2019-09-27 15:36:48 +0200

    added forgot password component

--- web/components/auth/ForgotPassword.js
+++ web/components/auth/ForgotPassword.js
@@ -2,3 +2,10 @@ import React, { useState } from 'react'
 import Auth from '@aws-amplify/auth'
 import { Alert, HeadingOne, Input, LinkButton, BodyText, DarkPinkButton } from 'umqombothi-component-library'
 
+const ForgotPassword = ({ props }) => {
+    return (
+        <>
+
+        </>
+    )
+}
