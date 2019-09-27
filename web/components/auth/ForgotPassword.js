c033692c5013c0f1d535d0a5ac0f625294b8bb41
AmoDinho <u14284783@tuks.co.za>
2019-09-27 15:43:45 +0200

    added state

--- web/components/auth/ForgotPassword.js
+++ web/components/auth/ForgotPassword.js
@@ -3,6 +3,16 @@ import Auth from '@aws-amplify/auth'
 import { Alert, HeadingOne, Input, LinkButton, BodyText, DarkPinkButton } from 'umqombothi-component-library'
 
 const ForgotPassword = ({ props }) => {
+    const [error, setError] = useState("")
+    const [email, setEmail] = useState("")
+    const [code, setCode] = useState("")
+    const [password, setPassword] = useState("")
+    const [codeSent, setCodeSent] = useState(false)
+    const [confirmed, setConfirmed] = useState(false)
+    //errors to display on RequestCodeForm
+    const [emailError, setEmailError] = useState("")
+    //errors to display on ConfirmationForm
+    const [confirmError, setConfirmError] = useState("")
     return (
         <>
 
