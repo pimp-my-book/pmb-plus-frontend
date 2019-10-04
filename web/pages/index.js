7beb1871815b85b5efe223c16965bad8cd932053
AmoDinho <u14284783@tuks.co.za>
2019-10-04 16:06:33 +0200

    added body text

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,4 +1,4 @@
-import { BodyText, HeadingOne } from 'umqombothi-component-library'
+import { HeadingFive, HeadingOne } from 'umqombothi-component-library'
 
 function Home() {
 
@@ -14,7 +14,15 @@ function Home() {
                     className="text-white"
                     text="Welcome to PMB Plus" />
 
-
+                <HeadingFive
+                    text={`You're here a bit early 😁`}
+                />
+                <HeadingFive
+                    text={`None the less it's great to have you here. Pimp My Book as 
+                          been passionate about helping students get their hands on cheap second hand books since forever.
+                          Hence we would like to intoduce a new service to make getting second hand books easier!😀 
+                    `}
+                />
             </div>
 
             <style jsx>
