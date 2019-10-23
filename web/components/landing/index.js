fc711bd905f71ee8b9a6a0f587799fb32af06b49
AmoDinho <u14284783@tuks.co.za>
2019-10-23 17:19:52 +0200

    import the two

--- web/components/landing/index.js
+++ web/components/landing/index.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import BooksByCouse from './BooksByCouse'
+import BooksByDegree from './BooksByDegree'
 
 const index = () => {
     return (
         <>
-
+            <BooksByDegree />
+            <BooksByCouse />
         </>
     )
 }
