145bca2298c3fc2522eafed97056cf61d1b26a64
AmoDinho <u14284783@tuks.co.za>
2019-10-23 17:23:46 +0200

    It is working

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,11 +1,11 @@
 import { BodyText } from 'umqombothi-component-library'
-
+import LandingPage from '../components/landing/index'
 function Home() {
 
 
     return (
         <>
-
+            <LandingPage />
 
         </>
     )
