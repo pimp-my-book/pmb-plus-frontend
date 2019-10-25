402654b133a05875d29ea9bd41306d71facc5fc8
AmoDinho <u14284783@tuks.co.za>
2019-10-25 12:13:34 +0200

    ADDED GET_BOOKS_BY_DEGREE query

--- web/graphql/Queries.js
+++ web/graphql/Queries.js
@@ -25,3 +25,15 @@ query GET_LATEST_BOOKS{
     }
 }
 `
+
+export const GET_BOOKS_BY_DEGREE = gql`
+query GET_BOOKS_BY_DEGREE{
+    getBooksByDegree{
+        ID
+        title
+        grade
+        price
+        image
+    }
+}
+`
