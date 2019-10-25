f42f1ea825a39a70ff8a3c31f8512bef450265cb
AmoDinho <u14284783@tuks.co.za>
2019-10-25 12:24:05 +0200

    added structuee to query page

--- web/components/landing/BooksByCourse.js
+++ web/components/landing/BooksByCourse.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react'
-
+import { useQuery } from '@apollo/react-hooks'
+import { GET_BOOKS_BY_COURSE } from '../../graphql/Queries'
 const BooksByCourse = () => {
+    const { loading, error, data } = useQuery(GET_BOOKS_BY_COURSE)
+    if (loading) return 'loading..'
+    if (error) return `${error.message}`
+
     return (
         <>
             <h1>Books by course</h1>
+            {data.getLatestBooks.map(book => (
+                <div key={book.ID}>
+                    {book.title}
+                </div>
+            ))}
         </>
     )
 }
