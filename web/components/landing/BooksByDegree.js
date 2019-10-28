d423a7f60e084512f14bdc348874f8a83712f90e
AmoDinho <u14284783@tuks.co.za>
2019-10-28 15:09:00 +0200

    added imports

--- web/components/landing/BooksByDegree.js
+++ web/components/landing/BooksByDegree.js
@@ -1,4 +1,10 @@
 import React, { useState } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { HeadingFive } from 'umqombothi-component-library'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import { GET_BOOKS_BY_DEGREE } from '../../graphql/Queries'
+import BookCard from '../card/BookCard'
 
 const BooksByDegree = () => {
     return (
