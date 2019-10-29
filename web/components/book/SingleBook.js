d550f0c0967e2d366ee79bb2113d400386f8aa95
AmoDinho <u14284783@tuks.co.za>
2019-10-29 17:53:59 +0200

    added comments

--- web/components/book/SingleBook.js
+++ web/components/book/SingleBook.js
@@ -22,17 +22,24 @@ const SingleBook = ({ }) => {
     if (loading) return 'loading..'
     if (error) return `${error.message}`
 
+    // get the data from the array provided by the api
+    const book = data.getOneBook
     return (
 
         <div>
             <BookGrid>
 
                 {/*START:Top left spot */}
-
+                <div>
+                    <img src={book.image} alt={`Image of ${book.title}`} />
+                </div>
                 {/*END: Top left spot */}
 
 
                 {/*START: Top Right*/}
+                <div>
+                    {book.title}
+                </div>
                 {/* END:Top Right*/}
 
                 {/*START: bottom left*/}
@@ -41,7 +48,7 @@ const SingleBook = ({ }) => {
                 {/*START: bottom Right*/}
                 {/* END: bottom Right*/}
             </BookGrid>
-            {data.getOneBook.title}
+
         </div>
 
     )
