6f0ab3c9205d494f26c46b64254870fdfc90199e
AmoDinho <u14284783@tuks.co.za>
2019-10-29 16:01:23 +0200

    added structure of query

--- web/components/book/SingleBook.js
+++ web/components/book/SingleBook.js
@@ -1,6 +1,18 @@
 import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import { useQuery } from '@apollo/react-hooks'
+import { GET_ONE_BOOK } from '../../graphql/Queries'
 
 const SingleBook = ({ }) => {
+
+    const router = useRouter()
+    const { id } = router.query
+    const { loading, data, error } = useQuery(GET_ONE_BOOK, {
+        variables: { ID: id }
+    })
+    if (loading) return 'loading..'
+    if (error) return `${error.message}`
+
     return (
         <>
 
