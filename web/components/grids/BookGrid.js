b84daaf303f862e42b67c9eec1565e91b2b9adf1
AmoDinho <u14284783@tuks.co.za>
2019-10-29 17:50:50 +0200

    added comments

--- web/components/grids/BookGrid.js
+++ web/components/grids/BookGrid.js
@@ -1,3 +1,9 @@
+/*
+This is a book grid component that uses CSS
+to handle the spacing of the data into a grid.
+It is also responsive
+*/
+
 import styled from 'styled-components'
 
 const BookGrid = styled.div`
@@ -5,3 +11,5 @@ display: grid;
 grid-template-columns: repeat(auto-fit,minmax(320px,1fr))
 
 `
+
+export default BookGrid
