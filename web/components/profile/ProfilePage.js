1a1c7cdf0a3213032379a5c0f58ece5575a46c5b
AmoDinho <u14284783@tuks.co.za>
2019-10-30 16:50:27 +0200

    added heading and image

--- web/components/profile/ProfilePage.js
+++ web/components/profile/ProfilePage.js
@@ -4,11 +4,18 @@ to their books and chats
 
 */
 import React from 'react'
+import { HeadingOne, HeadingTwo, HeadingFive, BodyText } from 'umqombothi-component-library'
+import Avatar from '../../assets/Avatar_Shape.svg'
 
 const ProfilePage = ({ }) => {
     return (
         <>
             <div>
+                <div>
+                    <HeadingOne text="User's name" />
+                    <img src={Avatar} alt="Avatar image" />
+
+                </div>
 
             </div>
         </>
