385d8546179393fa6f8dcbb18c6bee6dca6ef2bb
AmoDinho <u14284783@tuks.co.za>
2019-10-30 16:48:03 +0200

    added comments

--- web/components/profile/ProfilePage.js
+++ web/components/profile/ProfilePage.js
@@ -1,11 +1,16 @@
+/*
+This is the user's Profile page that will allow them to navigate 
+to their books and chats
+
+*/
 import React from 'react'
 
 const ProfilePage = ({ }) => {
     return (
         <>
             <div>
-                Your profile
-        </div>
+
+            </div>
         </>
     )
 }
