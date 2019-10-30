0aa688450000d3e96d921f6563d45b96b77514ee
AmoDinho <u14284783@tuks.co.za>
2019-10-30 16:31:59 +0200

    imported profile page

--- web/pages/profile.js
+++ web/pages/profile.js
@@ -1,22 +1,22 @@
 import Router from 'next/router'
 import Auth from "@aws-amplify/auth";
 import Cookie from 'js-cookie'
-
+import ProfilePage from '../components/profile/ProfilePage'
 const profile = (props) => {
     //console.log(props)
     const hasCookie = Cookie.get('token')
     if (!hasCookie) {
         return (
             <div>
-                You need to be logged in
+                <ProfilePage />
             </div>
 
         )
     } else {
         return (
-            <div>
-                Your profile
-            </div>
+            <>
+                <ProfilePage />
+            </>
         )
     }
 
