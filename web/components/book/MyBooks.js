181df1bb68b10a284f982da09ebafcbbf8fc4999
AmoDinho <u14284783@tuks.co.za>
2019-10-31 15:35:48 +0200

    Got to figure this out

--- web/components/book/MyBooks.js
+++ web/components/book/MyBooks.js
@@ -14,49 +14,28 @@ import { GET_MY_BOOKS } from '../../graphql/Queries'
 import Edit from '../../assets/edit.svg'
 import BookModal from './BookModal'
 import FormGrid from '../FormGrid'
-const MyBooks = () => {
+import { GET_ONE_BOOK } from '../../graphql/Queries'
 
-    //state
-    const [show, setShow] = useState(false)
 
-    const [targetID, setTargetID] = useState(0)
-    //query hook
-    const { loading, data, error } = useQuery(GET_MY_BOOKS, {
-        variables: { owner: '94c3ae75-5a32-4c44-bc17-e80cbfc006a7' }
+const RenderModal = (targetID) => {
+
+
+    //query hook - for geting one book
+
+    const { loading, data, error } = useQuery(GET_ONE_BOOK, {
+        variables: { ID: targetID }
     })
 
-    if (loading) return 'loading...'
+    if (loading) return 'loading your book...'
     if (error) return `${error.message}`
 
-    const books = data.getMyBooks
-
-
-    //Fucntions to handle closing and opening of modal
-    const handleClose = () => setShow(false)
-    const handleShow = (ID) => {
-        setShow(true)
-        setTargetID(ID)
-    }
+    const editableBook = data.getOneBook
     return (
         <>
-            <div>
-                <HeadingOne text=" My books" />
-
-                {
-                    books.map(book => (
-                        (
-                            <div className="flex flex-row p-10">
-                                <img className="w-24 h-24 mr-10" src={book.image} alt="book image" />  <HeadingFive className="mr-10" text={book.title ? book.title : 'Blank Title'} />  <img src={Edit} alt="edit icon" onClick={() => handleShow(book.ID)} />
-                            </div>
-                        )
-                    )
-                    )
-                }
-            </div>
-            {show && <BookModal
+            <BookModal
                 show={show}
-                onHide={handleClose}
-            >
+                onHide={handleClose}>
+
                 <div className="flex justify-center mb-20">
                     <form>
                         <HeadingOne
@@ -306,7 +285,52 @@ const MyBooks = () => {
                         {/* end of bottom form container */}
                     </form>
                 </div>
-            </BookModal>}
+            </BookModal>
+        </>
+    )
+}
+const MyBooks = () => {
+
+    //state
+    const [show, setShow] = useState(false)
+
+    const [targetID, setTargetID] = useState(0)
+    //query hook - for users books
+    const { loading, data, error } = useQuery(GET_MY_BOOKS, {
+        variables: { owner: '94c3ae75-5a32-4c44-bc17-e80cbfc006a7' }
+    })
+
+    if (loading) return 'loading...'
+    if (error) return `${error.message}`
+
+    const books = data.getMyBooks
+
+
+
+    //Fucntions to handle closing and opening of modal
+    const handleClose = () => setShow(false)
+    //Here we need to set the ID of the book in order to feed it to the query
+    const handleShow = (ID) => {
+        setShow(true)
+        setTargetID(ID)
+    }
+    return (
+        <>
+            <div>
+                <HeadingOne text=" My books" />
+
+                {
+                    books.map(book => (
+                        (
+                            <div className="flex flex-row p-10">
+                                <img className="w-24 h-24 mr-10" src={book.image} alt="book image" />  <HeadingFive className="mr-10" text={book.title ? book.title : 'Blank Title'} />  <img src={Edit} alt="edit icon" onClick={() => handleShow(book.ID)} />
+                            </div>
+                        )
+                    )
+                    )
+                }
+            </div>
+            {show && <RenderModal targetID={targetID} />}
 
         </>
 
