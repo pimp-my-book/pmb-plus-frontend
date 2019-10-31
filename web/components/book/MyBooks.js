e52eb4a6302cd844932ff2a64e249df5a3328f3e
AmoDinho <u14284783@tuks.co.za>
2019-10-31 11:27:55 +0200

    set up the query

--- web/components/book/MyBooks.js
+++ web/components/book/MyBooks.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { GET_MY_BOOKS } from '../../graphql/Queries'
 
 const MyBooks = () => {
+    const { loading, data, error } = useQuery(GET_MY_BOOKS, {
+        variables: { ID: '94c3ae75-5a32-4c44-bc17-e80cbfc006a7' }
+    })
+
+    if (loading) return 'loading...'
+    if (error) return `${error.message}`
+
+    const books = data.getMyBooks
     return (
         <>
             <div>
                 My books
-        </div>
+                {
+                    Object.keys(books).map((item, index) => {
+                        let values = result[item]
+                        return (
+                            <div key={index}>
+                                {
+                                    values.map(myBooks => (
+                                        {
+                                            myBooks.title
+                                        }
+                                    ))
+                                }
+                            </div>
+                        )
+                    })
+                }
+            </div>
 
         </>
     )
