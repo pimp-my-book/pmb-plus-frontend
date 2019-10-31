38443093db7e84225757a70aea501d62469734c2
AmoDinho <u14284783@tuks.co.za>
2019-10-31 12:04:50 +0200

    added edit icon

--- web/components/book/MyBooks.js
+++ web/components/book/MyBooks.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { HeadingOne, HeadingTwo, HeadingFive, BodyText } from 'umqombothi-component-library'
 import { GET_MY_BOOKS } from '../../graphql/Queries'
-
+import Edit from '../../assets/edit.svg'
 const MyBooks = () => {
     const { loading, data, error } = useQuery(GET_MY_BOOKS, {
         variables: { owner: '94c3ae75-5a32-4c44-bc17-e80cbfc006a7' }
@@ -20,7 +20,7 @@ const MyBooks = () => {
                     books.map(book => (
                         (
                             <div>
-                                <HeadingTwo text={book.title} />
+                                <HeadingTwo text={book.title} />  <img src={Edit} alt="edit icon" />
                             </div>
                         )
                     )
