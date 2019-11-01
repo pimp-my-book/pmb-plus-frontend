94dbc3cf21b277b501c284c2e6f87570ed20609b
AmoDinho <u14284783@tuks.co.za>
2019-11-01 12:49:43 +0200

    NEED add mutation for edit book

--- web/components/book/MyBooks.js
+++ web/components/book/MyBooks.js
@@ -11,7 +11,7 @@ To be able to render the modal the component needs to be split up into two
 */
 
 import React, { useState } from 'react'
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 import { HeadingOne, HeadingThree, HeadingFive, BodyText, Input, Textarea, DarkPinkButton } from 'umqombothi-component-library'
 import { GET_MY_BOOKS } from '../../graphql/Queries'
 import Edit from '../../assets/edit.svg'
@@ -32,6 +32,7 @@ const MyBooks = () => {
         variables: { owner: '94c3ae75-5a32-4c44-bc17-e80cbfc006a7' }
     })
 
+    const [editBook] = useMutation()
     if (loading) return 'loading...'
     if (error) return `${error.message}`
 
