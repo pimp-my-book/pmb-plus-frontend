f0ed4511fa20e6ee4ef010f886c3833d076a3b54
AmoDinho <u14284783@tuks.co.za>
2019-11-06 15:21:05 +0200

    added Category component

---
+++ web/components/search/Category.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Category = () => {
+    return (
+        <>
+            This page will show all the categories for a certain book by univeristy
+        </>
+    )
+
+
+}
+
+export default Category
