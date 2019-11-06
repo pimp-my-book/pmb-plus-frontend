f6700ab9e15ed1ef8d1e619bb74f5b24a4e5f72f
AmoDinho <u14284783@tuks.co.za>
2019-11-06 15:34:56 +0200

    adding comments

--- web/components/search/SearchHome.js
+++ web/components/search/SearchHome.js
@@ -1,4 +1,14 @@
+/*
+This is the search home page.
 
+*There is a search bar that will allow a user to search for any
+books
+
+*Then there is a grid that will group titles
+by their univeristy and allow the user to navigate
+to the page of books for that Univeristy
+
+*/
 import React from 'react'
 import { HeadingOne, HeadingThree } from 'umqombothi-component-library'
 const SearchHome = () => {
