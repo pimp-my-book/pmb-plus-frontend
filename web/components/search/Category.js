4ed99bc0a467f8d75c8cb677b83357025b764a4d
AmoDinho <u14284783@tuks.co.za>
2019-11-07 12:40:56 +0200

    all the books are loading

--- web/components/search/Category.js
+++ web/components/search/Category.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import { useQuery } from '@apollo/react-hooks'
 import { HeadingOne, HeadingTwo, HeadingFive, BodyText } from 'umqombothi-component-library'
+import BookCard from '../card/BookCard'
 
 import { GET_BOOKS_AT_A_UNIVERSITY } from '../../graphql/Queries'
 const Category = () => {
@@ -11,11 +12,28 @@ const Category = () => {
         variables: { university: uni }
     })
 
+    if (loading) return `Busy getting books from ${uni}`
+    if (error) return `${error.message}`
     console.log(data)
+
+    const books = data.getBooksAtAUniversity
     return (
         <>
             <HeadingOne text={`Books from ${uni} `} />
-
+            {
+                books.map(book => {
+                    return (
+                        <div>
+                            <BookCard
+                                grade={book.grade}
+                                img={book.image}
+                                price={book.price}
+                                title={book.title}
+                            />
+                        </div>
+                    )
+                })
+            }
         </>
     )
 
@@ -23,3 +41,4 @@ const Category = () => {
 }
 
 export default Category
+
