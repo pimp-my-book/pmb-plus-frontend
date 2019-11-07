2d51006c784a60a5c0936b0cc8559e64bd03154b
AmoDinho <u14284783@tuks.co.za>
2019-11-07 15:53:29 +0200

    added comments

--- web/components/search/SearchBar.js
+++ web/components/search/SearchBar.js
@@ -1,3 +1,11 @@
+/*
+This component is meant to allow the user to search
+for books via the title/isbn or author
+
+It uses Downshift for its Render Prop API
+
+*/
+
 import React from 'react'
 import { useQuery, ApolloConsumer } from '@apollo/react-hooks'
 import { Input } from 'umqombothi-component-library'
