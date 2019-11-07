145a7b3f34cea48710475ab0be8c23142b7afdf6
AmoDinho <u14284783@tuks.co.za>
2019-11-07 15:49:24 +0200

    created a SearchBar component

---
+++ web/components/search/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Input } from 'umqombothi-component-library'
+
+const SearchBar = () => {
+    return (
+        <>
+            <Input
+
+                placeholder="Search by title, ISBN or author"
+            />
+        </>
+    )
+}
+
+export default SearchBar
