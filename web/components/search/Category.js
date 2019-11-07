2cc519f6afaf8945507e4122a60504212f85289c
AmoDinho <u14284783@tuks.co.za>
2019-11-07 12:12:15 +0200

    managed to destructure the url query

--- web/components/search/Category.js
+++ web/components/search/Category.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 const Category = ({ books }) => {
+    const router = useRouter()
+
+    const { uni } = router.query
+    console.log(uni)
     return (
         <>
             This page will show all the categories for a certain book by univeristy
