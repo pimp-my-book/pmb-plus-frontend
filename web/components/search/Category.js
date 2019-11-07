d5710b6fa0e61052621164fc0b317dffe654b49e
AmoDinho <u14284783@tuks.co.za>
2019-11-07 12:26:47 +0200

    its all working

--- web/components/search/Category.js
+++ web/components/search/Category.js
@@ -4,9 +4,12 @@ import { useQuery } from '@apollo/react-hooks'
 import { GET_BOOKS_AT_A_UNIVERSITY } from '../../graphql/Queries'
 const Category = ({ books }) => {
     const router = useRouter()
-
     const { uni } = router.query
-    console.log(uni)
+    const { loading, data, error } = useQuery(GET_BOOKS_AT_A_UNIVERSITY, {
+        variables: { university: uni }
+    })
+
+    console.log(data)
     return (
         <>
             This page will show all the categories for a certain book by univeristy
