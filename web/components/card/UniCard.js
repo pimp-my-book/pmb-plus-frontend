b54c184e5f835fd870e00c52441bc09f25a0a989
AmoDinho <u14284783@tuks.co.za>
2019-11-08 11:57:01 +0200

    crated uniCard

---
+++ web/components/card/UniCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { HeadingFive } from 'umqombothi-component-library'
+
+const UniCard = ({ uniName }) => {
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
+
+export default UniCard
