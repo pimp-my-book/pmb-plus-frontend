0331f95ecf8ae7e0982f168438d894485b655d5c
AmoDinho <u14284783@tuks.co.za>
2019-11-11 12:07:02 +0200

    added padding

--- web/components/card/BookCard.js
+++ web/components/card/BookCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HeadingFive } from 'umqombothi-component-library'
+import location from '../../assets/location_on.svg'
 const BookCard = ({
     grade,
     img,
@@ -11,10 +12,13 @@ const BookCard = ({
             <div>
                 <img src={img} alt="book image" className="w-full h-86 " />
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col p-2">
                 <HeadingFive text={title} />
                 <HeadingFive text={`R ${price}`} />
                 <HeadingFive className="text-green" text={`Grade ${grade}`} />
+                <div className="flex flex-row">
+                    <img src={location} alt="location img" />
+                </div>
             </div>
 
         </div>
