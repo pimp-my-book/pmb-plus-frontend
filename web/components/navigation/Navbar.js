1ca19be4731884e98bbd146f61dba86f474d60d4
AmoDinho <u14284783@tuks.co.za>
2019-11-11 16:09:32 +0200

    created navigation bar

---
+++ web/components/navigation/Navbar.js
@@ -0,0 +1,53 @@
+/*
+NAVIGATION BAR
+FUNCTIONS:
+---on click it changes the state of isMenuOpen
+---Props
+* classname
+*children - to accept Additional JSX
+* content to accepts menu items in the nav drawer
+*/
+import React, { useState } from 'react';
+import { Navbar, Nav } from 'react-bootstrap'
+import styled from 'styled-components'
+import PropTypes from 'prop-types';
+import '../../index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavBarStyles = styled(Navbar)`
+&&&{
+    border-top: #ED0677 8px solid;
+}
+`
+const NavigationBar = ({
+    className = '',
+    children,
+    content
+}) => {
+
+
+    return (
+        <NavBarStyles
+            className=' bg-blueDarkest flex items-center justify-between flex-wrap bg-green-darker p-6'
+            expand="lg">
+            <Navbar.Brand> <img
+                src="https://pmb-plus-assets.s3.amazonaws.com/pmb_plus_logo.svg"
+                alt="PMB +"
+            /></Navbar.Brand>
+            <Navbar.Toggle><img
+                aria-label="collapse-menu"
+                src="https://pmb-static-assests.s3.amazonaws.com/menu.svg"
+                alt="menu"
+            /></Navbar.Toggle>
+            <Navbar.Collapse>
+                <Nav className="" aria-label="collapse-items">
+                    {children}
+                </Nav>
+            </Navbar.Collapse>
+        </NavBarStyles>
+
+    )
+}
+
+
+export default NavigationBar
