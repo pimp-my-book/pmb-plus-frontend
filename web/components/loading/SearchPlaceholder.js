f05e7db38f10d30dc4feb5172b421ffc4de9acd1
AmoDinho <u14284783@tuks.co.za>
2019-11-12 15:56:08 +0200

    added reactangle

--- web/components/loading/SearchPlaceholder.js
+++ web/components/loading/SearchPlaceholder.js
@@ -4,7 +4,9 @@ import ContentLoader from 'react-content-loader'
 const SearchPlaceholder = () => {
     return (
 
-        <ContentLoader></ContentLoader>
+        <ContentLoader>
+            <rect x="0" y="0" rx="5" ry="5" width="100" height="70" />
+        </ContentLoader>
     )
 }
 export default SearchPlaceholder
