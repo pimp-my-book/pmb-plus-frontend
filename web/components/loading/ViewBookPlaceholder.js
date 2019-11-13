adcbe1d3258f97cece0c46186440615da645b20b
AmoDinho <u14284783@tuks.co.za>
2019-11-13 14:10:10 +0200

    added viewbook placeholder

---
+++ web/components/loading/ViewBookPlaceholder.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ContentLoader from 'react-content-loader'
+
+const SearchPlaceholder = () => {
+    return (
+
+        <ContentLoader>
+        </ContentLoader>
+    )
+}
+export default SearchPlaceholder
