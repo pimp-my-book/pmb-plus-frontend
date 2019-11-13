de690b9a8f18149fd53f31c6b8ae1f6521cc29dc
AmoDinho <u14284783@tuks.co.za>
2019-11-13 14:15:28 +0200

    added content

--- web/components/loading/ViewBookPlaceholder.js
+++ web/components/loading/ViewBookPlaceholder.js
@@ -5,6 +5,11 @@ const SearchPlaceholder = () => {
     return (
 
         <ContentLoader>
+            <rect x="20" y="8" rx="0" ry="0" width="100" height="100" />
+            <rect x="20" y="120" rx="0" ry="0" width="100" height="10" />
+            <rect x="170" y="8" rx="0" ry="0" width="300" height="15" />
+            <rect x="170" y="30" rx="0" ry="0" width="300" height="15" />
+            <rect x="170" y="52" rx="0" ry="0" width="100" height="15" />
         </ContentLoader>
     )
 }
