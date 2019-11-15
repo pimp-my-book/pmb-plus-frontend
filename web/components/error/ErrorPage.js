8aa9855fd0eb4b278b855cb2eeb9226937d86163
AmoDinho <u14284783@tuks.co.za>
2019-11-15 12:18:10 +0200

    done with error component

--- web/components/error/ErrorPage.js
+++ web/components/error/ErrorPage.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import { HeadingOne } from 'umqombothi-component-library'
+import ErrorPic from '../../assets/fogg-fatal-error-3.png'
 const ErrorPage = () => {
     return (
         <>
+            <div>
+                <HeadingOne text="Something went wrong here, we are on it and getting it sorted" />
+                <img src={ErrorPic} alt="error pic" />
+
+            </div>
 
         </>
     )
