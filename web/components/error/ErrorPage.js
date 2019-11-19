16ef6eba0f137515fe21782d74388eaa44efe754
AmoDinho <u14284783@tuks.co.za>
2019-11-19 12:48:31 +0200

    its working

--- web/components/error/ErrorPage.js
+++ web/components/error/ErrorPage.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { HeadingOne } from 'umqombothi-component-library'
+import { HeadingThree } from 'umqombothi-component-library'
 import ErrorPic from '../../assets/later.png'
 const ErrorPage = () => {
     return (
         <>
-            <div>
-                <HeadingOne text="Something went wrong here, we are on it and getting it sorted" />
-                <img src={ErrorPic} alt="error pic" />
+            <div className="flex flex-col text-center">
+                <HeadingThree text="Something went wrong here, we are on it and getting it sorted" />
+                <img src={ErrorPic} alt="error pic" className="h-750" />
 
             </div>
 
