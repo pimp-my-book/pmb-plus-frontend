da2e59c6ffec91a1a28bdd4470a5c878df240690
AmoDinho <u14284783@tuks.co.za>
2019-11-26 15:38:20 +0200

    removed styled components import

--- web/components/book/AddBook.js
+++ web/components/book/AddBook.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
 import { Storage } from 'aws-amplify'
 import { useMutation, Mutation } from '@apollo/react-hooks';
 import Cookie from 'js-cookie'
