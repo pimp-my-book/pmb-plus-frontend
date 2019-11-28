60e3528387a692b13641c7e326158289be1afab6
AmoDinho <u14284783@tuks.co.za>
2019-11-28 15:08:01 +0200

    moove the link inside the card

--- web/components/card/BookCard.js
+++ web/components/card/BookCard.js
@@ -12,10 +12,12 @@ grade,
 */
 
 import React from 'react'
+import Link from 'next/link'
 import { HeadingFive, BodyText } from 'umqombothi-component-library'
 import locationIcon from '../../assets/location_on.svg'
 import placeholer from '../../assets/placeholder_image.svg'
 const BookCard = ({
+    ID,
     grade,
     img,
     price,
@@ -23,20 +25,25 @@ const BookCard = ({
     location
 }) => {
     return (
-        <div className="w-300 h-490 shadow cursor-pointer mb-5">
-            <div>
-                <img src={img ? img : placeholer} alt="book image" className="w-full h-64 " />
-            </div>
-            <div className="flex flex-col p-2">
-                <HeadingFive text={title} />
-                <HeadingFive text={`R ${price}`} />
-                <HeadingFive className="text-green" text={`Grade ${grade}`} />
-                <div className="flex flex-row">
-                    <img src={locationIcon} alt="location img" className="mb-8" /> <BodyText text={location ? location : 'X'} />
+        <Link
+            className="cursor-pointer"
+            href='/viewBook/id'
+            as={`/viewBook/${book.ID}`}>
+            <div className="w-300 h-490 shadow cursor-pointer mb-5">
+                <div>
+                    <img src={img ? img : placeholer} alt="book image" className="w-full h-64 " />
                 </div>
-            </div>
+                <div className="flex flex-col p-2">
+                    <HeadingFive text={title} />
+                    <HeadingFive text={`R ${price}`} />
+                    <HeadingFive className="text-green" text={`Grade ${grade}`} />
+                    <div className="flex flex-row">
+                        <img src={locationIcon} alt="location img" className="mb-8" /> <BodyText text={location ? location : 'X'} />
+                    </div>
+                </div>
 
-        </div>
+            </div>
+        </Link>
     )
 }
 export default BookCard
