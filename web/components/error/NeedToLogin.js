93dcd383feee8083b684e53eed5df09c5c045928
AmoDinho <u14284783@tuks.co.za>
2019-11-29 16:23:40 +0200

    semi-done with NeedToLogin component

--- web/components/error/NeedToLogin.js
+++ web/components/error/NeedToLogin.js
@@ -3,6 +3,10 @@ import Link from 'next/link'
 import Login from '../../assets/Login.png'
 const NeedToLogin = () => {
     return (
-
+        <>
+            <img src={Login} alt="you need to login" />
+        </>
     )
 }
+
+export default NeedToLogin
