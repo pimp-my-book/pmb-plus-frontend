064ae24439143974ddd40aae7edbdaa68e219948
AmoDinho <u14284783@tuks.co.za>
2019-11-29 16:39:59 +0200

    I'm satistfied

--- web/components/error/NeedToLogin.js
+++ web/components/error/NeedToLogin.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
+import { HeadingTwo, LinkButton } from 'umqombothi-component-library'
 import Login from '../../assets/Login.png'
 const NeedToLogin = () => {
     return (
         <>
-            <img src={Login} alt="you need to login" />
+            <div className="flex flex-col text-center mt-5">
+                <HeadingTwo text="A world of possibilities awaits you, but you need to login first" />
+                <div><LinkButton text="Let me login" href="/signin" /></div>
+                <img src={Login} alt="you need to login" />
+
+            </div>
+
         </>
     )
 }
