64115860046222767b60af05f0ed69370f751186
AmoDinho <u14284783@tuks.co.za>
2019-12-09 15:13:21 +0200

    lets see what the node_env is in dev

--- web/lib/initApollo.js
+++ web/lib/initApollo.js
@@ -21,6 +21,7 @@ function create(initialState, getToken) {
     // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
     const isBrowser = typeof window !== 'undefined'
 
+    console.log(process.env.NODE_ENV)
     //process.env.NODE_ENV === 'development' ? process.env.serviceEndpoint_PROD :
     //'http://localhost:4000/graphql'
     const httpLink = createHttpLink({
